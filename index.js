993073e716ceae82d6ca7cf48ed49b643c43e86b
Tomi Javanainen <tomi.javanainen@gmail.com>
2020-06-19 14:02:11 +0300

    PORT-muuttujan päivitys Herokulle sopivaksi

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ app.get('/api/persons', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
