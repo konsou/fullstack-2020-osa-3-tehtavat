f3e10b30d74932035708488f515e71848788caac
Tomi Javanainen <tomi.javanainen@gmail.com>
2020-06-19 20:10:09 +0300

    frontend-build

---
+++ build/precache-manifest.49d377888ad3754fa1baac01d80bd526.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2275150b46eb8f064fd6c137a08e7329",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09ebe5ea41d0da014ace",
+    "url": "/static/css/main.32a5f425.chunk.css"
+  },
+  {
+    "revision": "c9a736b30335e4a7f81c",
+    "url": "/static/js/2.42f56bda.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.42f56bda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09ebe5ea41d0da014ace",
+    "url": "/static/js/main.197ffe27.chunk.js"
+  },
+  {
+    "revision": "24aecb121d5a1e31ca85",
+    "url": "/static/js/runtime-main.455ce434.js"
+  }
+]);
